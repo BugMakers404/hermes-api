22bc98746162704fb219aa0c6260bd3f8d1ca570
tyriongump <tyriongump@gmail.com>
2023-05-16 23:14:56 +1000

    init hermes api project

---
+++ hermes-api-main/src/main/java/org/bugmakers404/hermes/api/configs/AsyncConfig.java
@@ -0,0 +1,41 @@
+package org.bugmakers404.hermes.api.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+@Slf4j
+public class AsyncConfig implements AsyncConfigurer{
+
+    @Value("${hermes.api.thread.maxpoolsize:2}")
+    Integer poolSize;
+
+    @Override
+    @Bean
+    public AsyncTaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+        if (poolSize <= 0) {
+            throw new IllegalArgumentException("The value of hermes.api.thread.maxpoolsize must be greater than 0");
+        }
+        pool.setCorePoolSize(poolSize / 2);
+        pool.setMaxPoolSize(poolSize);
+        pool.setThreadNamePrefix("HAExec-");
+        pool.setThreadPriority(Thread.NORM_PRIORITY);
+        pool.initialize();
+        return pool;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> log.error("Async method {} threw an exception: {}", method.getName(),
+                ex.getMessage(), ex);
+    }
+}
